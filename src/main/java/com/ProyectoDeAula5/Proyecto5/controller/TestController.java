5f99ae5654ea2d42cb48f872fe940b61fe85cffc
Tu Nombre <tucorreo@ejemplo.com>
2025-05-20 09:50:16 -0500

    test2

--- src/main/java/com/ProyectoDeAula5/Proyecto5/controller/TestController.java
+++ src/main/java/com/ProyectoDeAula5/Proyecto5/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.ProyectoDeAula5.Proyecto5.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 @RestController
 @RequestMapping("/api")
 public class TestController {
