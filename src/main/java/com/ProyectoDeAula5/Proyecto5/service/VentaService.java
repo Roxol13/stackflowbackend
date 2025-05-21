8987d041f49eb313bee3c8e2c7673d4700195616
Tu Nombre <tucorreo@ejemplo.com>
2025-05-21 16:12:37 -0500

    cambio de venta controller3

--- src/main/java/com/ProyectoDeAula5/Proyecto5/service/VentaService.java
+++ src/main/java/com/ProyectoDeAula5/Proyecto5/service/VentaService.java
@@ -42,10 +42,14 @@ public class VentaService {
         List<DetalleVenta> detalles = new ArrayList<>();
         if (venta.getDetallesVenta() != null) {
             for (DetalleVenta detalle : venta.getDetallesVenta()) {
-                log.info("Guardando detalles de la venta " + detalle);
-                productoService.actualizarInventario(detalle.getCod_pro(), detalle.getCantidad());
-                detalles.add(guardarDetalleVenta(detalle));
+            detalle.setVenta(venta); // ❗ Relación inversa
+            productoService.actualizarInventario(detalle.getCod_pro(), detalle.getCantidad());
+            detalles.add(guardarDetalleVenta(detalle));
             }
+        } else {
+            log.error("No se han proporcionado detalles de venta.");
+            return null; // O lanzar una excepción
+
         }
 
         venta.setDetallesVenta(detalles);
